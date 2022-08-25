8be9923a41556874a8b7a2d4ce5842621fa33395
iasminfukushima <iasmin_fukushima@hotmail.com>
2022-08-25 16:37:31 -0300

    primeiro programa mensagem viagem

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,6 @@
-nome = Iasmin
-email = iasmin_fukushima@hotmail.com
-profissão = Pedagoga
-idade = 24
-localsonhado = Japão
+let nome = "Iasmin"
+let email = "iasmin_fukushima@hotmail.com"
+let profissão = "Pedagoga"
+let idade = 24
+let localsonhado = "Japão"
+console.log("Olá,", nome, "!", "Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para", localsonhado, "Confirma pra gente alguns dados? A sua idade é", idade, "você é",  profissão, "e o seu email é", email, "? Ficamos no aguardo. Muito obrigado e boa viagem! :D")
